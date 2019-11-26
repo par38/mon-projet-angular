3c87b54a45ba13fef72eae138aab017f5552c6c6
Monica TARABUSI <code.mta@gmail.com>
2019-11-26 19:44:39 +0100

    FormModule ReactiveFormsModule FormGroup FormBuilder Subject Subscription OnDestroy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { Routes, RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
@@ -13,6 +13,9 @@ import { SingleAppareilComponent } from './single-appareil/single-appareil.compo
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component'
 import {AuthGuard} from './services/auth-guard.service';
 import { EditAppareilComponent } from './edit-appareil/edit-appareil.component'
+import { UserService } from './services/user.service';
+import { UserListComponent } from './user-list/user-list.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
 const appRoutes: Routes = [
   // ++++++ attention : les routes sont regardées dans l'ordre ! mettre la wildcard en dernière 
@@ -20,6 +23,8 @@ const appRoutes: Routes = [
   { path: 'appareils/:id', canActivate: [AuthGuard], component: SingleAppareilComponent },
   { path: 'edit', canActivate: [AuthGuard], component: EditAppareilComponent }, 
   { path: 'auth', component: AuthComponent },
+  { path: 'users', component: UserListComponent },
+  {path: 'new-user', component: NewUserComponent},
   { path: '', component: AuthComponent },
   // { path: '', component: AppareilViewComponent },
   { path: 'not-found', component: FourOhFourComponent },
@@ -37,17 +42,21 @@ const appRoutes: Routes = [
     AppareilViewComponent,
     SingleAppareilComponent,
     FourOhFourComponent,
-    EditAppareilComponent
+    EditAppareilComponent,
+    UserListComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule
   ],
   providers: [
     AppareilService,
     AuthService, 
-    AuthGuard
+    AuthGuard,
+    UserService
   ],
   bootstrap: [AppComponent]
 })
